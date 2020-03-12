6c7f25dce6a7e3f4e0f785684b522d251610e4a4
Ramiro Cedillo <1730091@upv.edu.mx>
2020-03-12 02:40:17 -0600

    adfasdf

---
+++ app/Http/Controllers/ConceptosController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\conceptos;
+use Illuminate\Http\Request;
+use App\Http\Requests\ConceptosRequest;
+use Illuminate\Support\Facades\Hash;
+
+class ConceptosController extends Controller
+{
+    /**
+     * Display a listing of the users
+     *
+     * @param  \App\trabajadores  $model
+     * @return \Illuminate\View\View
+     */
+    public function index(conceptos $model)
+    {
+        return view('conceptos.index', ['conceptos' => $model->paginate(15)]);
+    }
+
+    /**
+     * Show the form for creating a new user
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('conceptos.create');
+    }
+
+    /**
+     * Store a newly created user in storage
+     *
+     * @param  \App\Http\Requests\TrabajadoresRequest  $request
+     * @param  \App\trabajadores  $model
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(ConceptosRequest $request, conceptos $model)
+    {
+        $model->create($request->all());
+        $a = array('fk_id_usuario' => $_COOKIE['ses'], 'accion' => 'Crear', 'lugar' => 'Concepto nuevo.');
+        historial::create($a);
+        return redirect()->route('conceptos.index')->withStatus(__('Concepto creado exitosamente.'));
+    }
+
+    /**
+     * Show the form for editing the specified user
+     *
+     * @param  id del trabajador $id
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $concepto=conceptos::find($id);
+        return view('conceptos.edit', compact('concepto'));
+    }
+
+    /**
+     * Update the specified user in storage
+     *
+     * @param  \App\Http\Requests\UserRequest  $request
+     * @param  \App\trabajadores  $trabajador
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(ConceptosRequest $request, conceptos  $concepto, $id)
+    {   
+        $concep =conceptos::findOrFail($id);
+        $concep->update($request->all());
+        $a = array('fk_id_usuario' => $_COOKIE['ses'], 'accion' => 'Editar', 'lugar' => 'Concepto con id: '.$id);
+        historial::create($a);
+        return redirect()->route('conceptos.index')->withStatus(__('Concepto modificado exitosamente.'));
+    }
+
+    /**
+     * Remove the specified user from storage
+     *
+     * @param  id del trabajador $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy( $id)
+    {
+        //$trabajador->delete();
+        try{
+            conceptos::find($id)->delete();
+            $a = array('fk_id_usuario' => $_COOKIE['ses'], 'accion' => 'Eliminar', 'lugar' => 'Concepto con id: '.$id);
+        historial::create($a);
+            return redirect()->route('conceptos.index')->withStatus(__('Concepto elimindado exitosamente.'));
+        }catch(Exception $ex){
+            //return response()->json([
+              //  'error' => 'Hubo un error al eliminar el registro con id: '.$id. ' : '.$ex->getMessage()
+            //], 400);
+        }
+        
+        
+    }
+}
---
+++ app/Http/Controllers/ProyectosController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\proyectos;
+use App\clientes;
+use App\trabajadores;
+use Illuminate\Http\Request;
+use App\Http\Requests\ProyectosRequest;
+use Illuminate\Support\Facades\Hash;
+
+class ProyectosController extends Controller
+{
+    /**
+     * Display a listing of the users
+     *
+     * @param  \App\trabajadores  $model
+     * @return \Illuminate\View\View
+     */
+    public function index(proyectos $model)
+    {
+        $proyectos= proyectos::join("clientes","proyectos.fk_id_cliente","=","clientes.id")->join("trabajadores","proyectos.fk_id_lider","=","trabajadores.id")->get();
+        //print_r(compact($proyectos));
+        return view('proyectos.index', ['proyectos' => $proyectos]);
+    }
+
+    /**
+     * Show the form for creating a new user
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        $clientes = clientes::all();
+        $trabajadores = trabajadores::where("puesto","=","Director de proyectos")->get();
+        //print_r($trabajadores);
+        $a = array('fk_id_usuario' => $_COOKIE['ses'], 'accion' => 'Creo', 'lugar' => 'Proyecto con id: '.$id);
+        historial::create($a );     
+        return view('proyectos.create', ['info' => $trabajadores], ['clientes' => $clientes] );
+    }
+
+    /**
+     * Store a newly created user in storage
+     *
+     * @param  \App\Http\Requests\TrabajadoresRequest  $request
+     * @param  \App\trabajadores  $model
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(ProyectosRequest $request, proyectos $model)
+    {
+        $model->create($request->all());
+        //echo("asdsdasd");
+        return redirect()->route('proyectos.index')->withStatus(__('Proyecto creado exitosamente.'));
+    }
+
+    /**
+     * Show the form for editing the specified user
+     *
+     * @param  id del trabajador $id
+     * @return \Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        $proyectos=proyectos::where("id","=",$id)->get();;
+        $cliente = clientes::all();
+        $trabajador = trabajadores::where("puesto","=","Director de proyectos")->get();
+        print_r(compact('proyectos'));
+        print_r($proyectos);
+        $a = array('fk_id_usuario' => $_COOKIE['ses'], 'accion' => 'Editar', 'lugar' => 'Proyecto con id: '.$id);
+        historial::create($a );
+        return view('proyectos.edit', compact('proyectos'), compact('trabajador','clientes'));
+    }
+
+    /**
+     * Update the specified user in storage
+     *
+     * @param  \App\Http\Requests\UserRequest  $request
+     * @param  \App\trabajadores  $trabajador
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(ProyectosRequest $request, proyectos  $proyectos, $id)
+    {   
+        $proy =proyectos::findOrFail($id);
+        $proy->update($request->all());
+        return redirect()->route('proyectos.index')->withStatus(__('Proyecto modificado exitosamente.'));
+    }
+
+    /**
+     * Remove the specified user from storage
+     *
+     * @param  id del trabajador $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy( $id)
+    {
+        //$trabajador->delete();
+        try{
+            proyectos::find($id)->delete();
+            $a = array('fk_id_usuario' => $_COOKIE['ses'], 'accion' => 'Eliminar', 'lugar' => 'Proyecto con id: '.$id);
+        historial::create($a );
+            return redirect()->route('proyectos.index')->withStatus(__('Proyecto elimindado exitosamente.'));
+        }catch(Exception $ex){
+            //return response()->json([
+              //  'error' => 'Hubo un error al eliminar el registro con id: '.$id. ' : '.$ex->getMessage()
+            //], 400);
+        }
+        
+        
+    }
+}
